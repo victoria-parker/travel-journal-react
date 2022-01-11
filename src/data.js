7c73dc1f4b0f14f3f3b73bbae6a3c601681729d6
victoria-parker <victoriaparkerb@gmail.com>
2022-01-11 11:31:50 +0000

    data.js done

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id:1,
+        title: "Mount Fuji",
+        location:"Japan",
+        googleMapLink:"https://goo.gl/maps/zHFzWDgLrXCZkWqb7",
+        startDate:new Date(2021,1,12),
+        endDate:new Date(2021,1,24),
+        description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imgUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    },
+    {
+        id:2,
+        title:"Sydney Opera House",
+        location:"Australia",
+        googleMapLink:"https://goo.gl/maps/rt5eYHpAy6UJsq2d8",
+        startDate: new Date(2021,5,27),
+        endDate:new Date(2021,6,8),
+        description:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imgUrl:"https://unsplash.com/photos/JmuyB_LibRo"
+    },
+    {
+        id:3,
+        title:"Geirangerfjord",
+        location:"Norway",
+        googleMapLink:"https://goo.gl/maps/6qnJAbhMXgwecFgZ8",
+        startDate:new Date(2021,10,1),
+        endDate:new Date(2021,11,18),
+        description:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imgUrl:"https://unsplash.com/photos/3PeSjpLVtLg"
+    }
+]
