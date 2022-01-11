8c66447b4f92742d5bb268b267d248eec6ea7a40
victoria-parker <victoriaparkerb@gmail.com>
2022-01-11 11:08:36 +0000

    card component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./style.css"
 import NavBar from "./components/NavBar"
+import Card from "./components/Card"
 
 export default function App(){
     return (
         <div className="container">
             <NavBar/>
+            <section className="cards">
+                <Card/>
+            </section>
         </div>
     )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Card(){
+    return (
+        <div className="card">
+            <div className="card-image">
+                image here
+            </div>
+            <div className="card-text">
+                text here
+            </div>
+        </div>
+    )
+}
